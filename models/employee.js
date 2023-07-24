4bf3f439f4bc6bd01c0828a50b0ffbcb2b0cd2ab
Shimmy Dasari <shimmyroy.dasari@bounteous.com>
2023-07-24 16:38:07 +0530

    Add ES Lint

--- models/employee.js
+++ models/employee.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-  eid: {type: Number, required: true},
+  eid: { type: Number, required: true },
   name: { type: String, required: true },
   age: { type: Number, required: true },
   gender: { type: String, required: true },
   skills: { type: [String], required: false },
   designation: { type: String, required: true },
-  userCredentialsId: {type: mongoose.Schema.Types.ObjectId, ref: 'UserCredentials'}
+  userCredentialsId: { type: mongoose.Schema.Types.ObjectId, ref: 'UserCredentials' }
 });
 
 const Employee = mongoose.model('Employee', employeeSchema);
--- models/userCredentials.js
+++ models/userCredentials.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userCredentialsSchema = new mongoose.Schema({
-    email: { 
-        type: String, 
+    email: {
+        type: String,
         required: true,
         // unique doesn't validate whether there's a duplicate. It just provides some performance benefits
         unique: true,
     },
-    password: { type: String, required: true}
+    password: { type: String, required: true }
 });
 
 const UserCredentials = mongoose.model('UserCredentials', userCredentialsSchema);
